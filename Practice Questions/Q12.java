0eabf0e9fa7a0c1aae61ef1abd905b797678b276
Prayush Gupta <pratyush.gupta.in@gmail.com>
2026-07-14 05:59:24 +0530

    Q12 done

--- Practice Questions/Q12.java
+++ Practice Questions/Q12.java
@@ -1,5 +1,27 @@
 // Write a program to calculate the area and perimeter of a rectangle given its length and width.
 
+import java.util.Scanner;
 public class Q12 {
-  
+  public static void main(String[] args) {
+    double width;
+    double height;
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the width:  ");
+    width = sc.nextDouble();
+    System.out.println("Enter the height:   ");
+    height = sc.nextDouble();
+    sc.close();
+
+    //Area
+    double area;
+    area = width*height;
+    //Perimeter
+    double perimeter;
+    perimeter = 2*(width+height);
+
+    System.out.println("=== Area & Perimeter of a Rectangle ===");
+    System.out.printf("Area: %.2f\n",  area);
+    System.out.printf("Perimeter: %.2f\n", perimeter);
+  }
 }
